3e14129f201398858ff4b6d96f003d69bb4e789a
willianbortolini <willian.borto@gmail.com>
2023-05-02 11:44:16 -0300

    colar sem formatacao e nova fonte

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-const { app, BrowserWindow, ipcMain,Menu  } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow({    
-    width: 800,
-    height: 600,
-    frame: true,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    },
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-      color: '#2f3241',
-      symbolColor: '#74b1be',
-      height: 33
-    }
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#2f3241',
+            symbolColor: '#74b1be',
+            height: 33
+        }
+    });
 
-  win.loadFile('index.html'); 
+    win.loadFile('index.html');
 
-  // Remover o menu padrão
-  //win.webContents.openDevTools();
-  
+    // Remover o menu padrão
+    //win.webContents.openDevTools();
+
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  ipcMain.on('close-window', (event) => {
-    const win = BrowserWindow.getFocusedWindow();
-    win.close();
-  });
+    ipcMain.on('close-window', (event) => {
+        const win = BrowserWindow.getFocusedWindow();
+        win.close();
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
